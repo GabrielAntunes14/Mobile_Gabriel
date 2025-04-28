672309e9a78d73431651117d953cf2827a409943
antunes <163547062+GabrielAntunes14@users.noreply.github.com>
2025-04-28 18:55:16 -0300

    Update _layout.tsx

--- Trabalho_Mobile/app/(tabs)/_layout.tsx
+++ Trabalho_Mobile/app/(tabs)/_layout.tsx
@@ -27,7 +27,7 @@ export default function TabLayout() {
       }}
     >
       <Tabs.Screen
-        name="inicio"
+        name="index"
         options={{
           title: 'Início',
           tabBarIcon: ({ color, size = 28 }) => (
